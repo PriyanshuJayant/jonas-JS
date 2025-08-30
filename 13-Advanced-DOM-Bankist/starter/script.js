ae691baabb3619baf4ad9d554f71dc2b3ea548e5
Priyanshu Jayant <priyanshujayant8@gmail.com>
2025-08-30 18:09:31 +0530

    Advance DOM

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,15 @@
 'use strict';
 
-///////////////////////////////////////
 // Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -17,6 +20,8 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
@@ -29,3 +34,121 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////////////////////////////////////////
+//   Page Navigation
+// document.querySelectorAll('.nav__link').forEach(function(el){
+//   el.addEventListener('click', function(e){
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+//   })
+// });
+
+//  1. Adding event listener to common parent element
+//  2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // Matching Strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return; // Guard clause
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate clicked tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate related content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+//  Button Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // Old Technique
+  // window.scrollTo(
+  // );
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // })
+  //  New Technique
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+
+//  Menu fade animation
+
+const handleHover = function(e, opacity){  
+    if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const sibling = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('nav').querySelector('img');
+    
+    sibling.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    })
+    // logo.stye.opacity = this;
+  }
+}
+
+//  Passing argument into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+//////////////////////////////////////////////////////////////////////////////////
+//  Sticky navigation
+const obsCallback = function(entries, observer){
+  entries.forEach(entry => {
+    console.log(entry);
+    
+  })
+};
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2]
+};
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+;
+observer.observe(section1);
+
+
+
+
+
+
+
+
+
+
+const initialCords = section1.getBoundingClientRect()
+// console.log(initialCords);
+
+
+window.addEventListener('scroll', function(){
+  // console.log(window.scrollY);
+  if(window.scrollY > initialCords.top){
+    nav.classList.add('sticky')
+  }else{
+    nav.classList.remove('sticky');
+  }
+  
+})
---
+++ 13-Advanced-DOM-Bankist/starter/tutorial.js
@@ -0,0 +1,61 @@
+// ------------------------Event Listener--------------------------//
+//  New Way of Listening Events
+const h1 = document.querySelector('h1');
+const alertH1 = function(e){
+  alert('You are a Nigger Bitch ass!!!!');
+  h1.removeEventListener('mouseenter',alertH1)
+}
+h1.addEventListener('mouseenter', alertH1);
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 5000)
+
+//  Old Way of Listening Events
+
+// const h4 = document.querySelector('h4');
+// h4.onmouseenter = (e) => {
+//   alert('You are Gay!!')
+// }
+
+//---------------------------Random Color Generator----------------------//
+// rgb(255,255,255)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0,255)},${randomInt(0,255)})`
+console.log(randomColor(0,255));
+
+document.querySelector('.nav__link').addEventListener('click', function(e){
+  this.style.backgroundColor = randomColor();
+  console.log(e.target, e.currentTarget);
+  e.stopPropagation();
+}, true)
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  this.style.backgroundColor = randomColor();
+  e.stopPropagation();
+})
+document.querySelector('.nav').addEventListener('click', function(e){
+this.style.backgroundColor = randomColor();
+})
+
+//------------------------------------------------------------------------//
+// const h1 = document.querySelector('h1');
+
+// going downward: child
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'white';
+
+// Going upwards: parent
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//  Going sideways: sibling
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+const element = [...h1.parentElement.children].forEach(function(el){
+  if (el !== h1) {el.style.transform = 'scale(0.5)'}
+})
+console.log(element);
