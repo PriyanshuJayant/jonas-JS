bddc1d7ee78587691fd8dfb283d24846a6a72c0f
Priyanshu Jayant <priyanshujayant8@gmail.com>
2025-09-01 01:23:16 +0530

    Advance Dom Complete

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -136,14 +136,13 @@ headerObserver.observe(header);
 ////////////////////////////////////////////////////////////////////////////
 //  Reveal Section
 
-
 const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer) {
   entries.forEach(entry => {
-    if(!entry.isIntersecting) return;
+    if (!entry.isIntersecting) return;
     entry.target.classList.remove('section--hidden');
     observer.unobserve(entry.target);
-  })
+  });
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -152,27 +151,98 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 });
 
 allSections.forEach(function (section) {
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
   sectionObserver.observe(section);
 });
 
 ////////////////////////////////////////////////////////////////////////////
 //  Lazy Loading img
 const imgTarget = document.querySelectorAll('img[data-src]');
-const loadImg = function(entries, observer){
+const loadImg = function (entries, observer) {
   const [entry] = entries;
-  console.log(entries);
-  if(!entry.isIntersecting) return;
+  if (!entry.isIntersecting) return;
   //Replace the src with data-src
   entry.target.src = entry.target.dataset.src;
-  entry.target.addEventListener('load', function(){
+  entry.target.addEventListener('load', function () {
     entry.target.classList.remove('lazy-img');
   });
   observer.unobserve(entry.target);
-}
+};
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
   rootMargin: '200px',
-})
+});
 imgTarget.forEach(img => imgObserver.observe(img));
+
+////////////////////////////////////////////////////////////
+// Slider Component
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
+let curSlide = 0;
+const maxSlide = slides.length;
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) curSlide = 0;
+  else curSlide++;
+  goToSlide(curSlide);
+  activateDot(curSlide);
+};
+const prevSlide = function () {
+  if (curSlide === 0) curSlide = maxSlide - 1;
+  else curSlide--;
+  goToSlide(curSlide);
+  activateDot(curSlide);
+};
+//  Event Handlers
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+});
+const createDots = function () {
+  slides.forEach((_, i) => {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide=${i}></button>`
+    );
+  });
+};
+createDots();
+const activateDot = function(slide){
+  document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+}
+activateDot(0);
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const curSlide = Number(e.target.dataset.slide);
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  }
+});
+////////////////////////////////////////////////////////////////////////////
+document.addEventListener('DOMContentLoaded', function(e){
+  // console.log('HTML Parsed and Dom tree built', e);
+  
+})
+
+window.addEventListener('load', (e) => {
+  // console.log('Page Loaded', e);
+})
+// window.addEventListener('beforeunload', (e)=> {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+//   alert('Are you sure??');
+// })
