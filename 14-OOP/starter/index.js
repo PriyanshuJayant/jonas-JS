8b033c8b4e78300625de41c8e3ead8a8fc8589bd
Priyanshu Jayant <priyanshujayant8@gmail.com>
2025-10-10 10:56:10 +0530

    OOPS Complete

--- 14-OOP/starter/index.js
+++ 14-OOP/starter/index.js
@@ -58,34 +58,113 @@
 // Student.prototype.constructor = Student;
 // console.dir(Student.prototype.constructor);
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going art ${this.speed} km/h`);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is going art ${this.speed} km/h`);
+// };
 
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed);
-  this.speed = speed;
-  this.charge = charge;
-};
+// const EV = function (make, speed, charge) {``
+//   Car.call(this, make, speed);
+//   this.speed = speed;
+//   this.charge = charge;
+// };
 
-// Link Prototype
-EV.prototype = Object.create(Car.prototype);
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-};
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge--;
-  console.log(`${this.make} is going art ${this.speed} km/h, with a charge of ${this.charge}`);
-};
+// // Link Prototype
+// EV.prototype = Object.create(Car.prototype);
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge--;
+//   console.log(`${this.make} is going art ${this.speed} km/h, with a charge of ${this.charge}`);
+// };
 
-const tesla = new EV('Tesla', 120, 23);
-tesla.chargeBattery(90);
-console.log(tesla);
-tesla.accelerate();
+// const tesla = new EV('Tesla', 120, 23);
+// tesla.chargeBattery(90);
+// console.log(tesla);
+// tesla.accelerate();
+
+// const PersonProto = {
+//     calcAge() {
+//         console.log(2025 - this.birthYear);
+//     },
+//     init(firstName, birthYear) {
+//         this.firstName = firstName;
+//         this.birthYear = birthYear;
+//     },
+// };
+
+// const steven = Object.create(PersonProto);
+// const StudentProto = Object.create(PersonProto);
+// StudentProto.init = function (firstName, birthYear, course) {
+//     PersonProto.init.call(this, firstName, birthYear);
+//     this.course = course;
+// }
+// StudentProto.introduce = function () {
+//     console.log(`My Name is ${this.fullName} and i study ${this.course}`);
+// }
+// const jay = Object.create(StudentProto);
+// jay.init('jay', 2000, 'Computer Science');
+// jay.introduce();
+// jay.calcAge();
+
+class Account {
+  locale = navigator.language;
+  bank = 'Bankist';
+
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+    // this.locale = navigator.language;
+  }
+
+  //  Public interface (API)
+  getMovements() {
+    return this.#movements;
+  }
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+  #approveLoan(vla) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan Approved');
+    }
+    return this;
+  }
+  static test() {
+    console.log('TEST');
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111, []);
+// acc1.deposit(250);
+// acc1.withdraw(140);
+const movements = acc1
+  .deposit(300)
+  .withdraw(100)
+  .withdraw(50)
+  .requestLoan(25000)
+  .withdraw(4000)
+  .getMovements();
+
+console.log(acc1);
+console.log(movements);
