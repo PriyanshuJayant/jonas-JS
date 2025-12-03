102b77d8933931e1ecd1a078ddbb44a7d31d6ab0
Priyanshu Jayant <priyanshujayant8@gmail.com>
2025-12-03 23:47:05 +0530

    auto

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,16 @@
+// Importing Module
+// import './shoppingCart.js';
+// import { addToCart, totalPrice as price, qt } from './shoppingCart.js';
+// addToCart('bread', 5);0
+// console.log(price, qt);
+
+// console.log(ShoppingCart);
+
+console.log('Importing Module');
+
+// import * as ShoppingCart from './shoppingCart.js'
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+import add from './shoppingCart.js'
+add('pizza', 2);
---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,20 @@
+//   Exporting module
+console.log('Exporting Module');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to Cart`);
+}
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as qt };
+
+export default function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to Cart`);
+}
